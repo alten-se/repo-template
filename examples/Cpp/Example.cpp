ffa6ae343ebe84a36cf7f14222e297066a2c5423
vishwas@ALTEN <vishwas.devagiri@alten.se>
2022-11-10 10:42:03 +0100

    googletest init

---
+++ examples/Cpp/Example.cpp
@@ -0,0 +1,5 @@
+#include "Example.hpp"
+
+bool f() {
+        return false;
+}
---
+++ examples/Cpp/test/ExampleTests.cpp
@@ -0,0 +1,33 @@
+#include <gtest/gtest.h>
+#include "Example"
+
+TEST(ExampleTests, DemonstrateGTestMacros)
+
+{
+    EXPECT_EQ(true, true);
+    const bool result = f();
+    EXPECT_EQ(true, result)<< "Hello world";
+
+
+}
+
+TEST(ExampleTests, DemonstrateGTestMacros2)
+
+{
+    EXPECT_EQ(true, true);
+    const bool result = f();
+    EXPECT_EQ(true, result)<< "Hello world";
+
+
+}
+
+TEST(ExampleTests, DemonstrateGTestMacros3)
+
+{
+    EXPECT_EQ(true, true);
+    const bool result = f();
+    EXPECT_EQ(true, result)<< "Hello world";
+
+
+}
+
